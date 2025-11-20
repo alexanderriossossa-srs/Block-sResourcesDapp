d38560ce30350872325649bc7836c1535c13215e
alexanderriossossa-srs <alexanderriossossa@gmail.com>
2025-11-20 10:25:54 -0500

    Update invoke.sh

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# Invoke log_trace
-soroban contract invoke \
+stellar contract invoke \
   --source <PUBLIC_MULTISIG> \
   --network testnet \
-  --wasm target/wasm32-unknown-unknown/release/block_resources.wasm \
+  --id <CONTRACT_ID> \
   log_trace \
-  -- <RESOURCE_ID> <FROM> <TO> '{ "tipo": "agua", "cantidad": "100L" }' \
+  -- b"RECURSO_001" <PUB_A> <PUB_RECEIVER> '{ "tipo": "agua", "cantidad": "100L" }' \
   --signer <SECRET_A> --signer <SECRET_B>  # Dos firmas
