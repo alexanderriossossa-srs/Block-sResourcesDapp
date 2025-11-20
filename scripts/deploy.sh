6dff40802c345a130b3a9fa11773f20a53be10f1
alexanderriossossa-srs <alexanderriossossa@gmail.com>
2025-11-20 10:24:37 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cargo build --target wasm32-unknown-unknown --release
-soroban contract deploy \
+stellar contract deploy \
   --wasm target/wasm32-unknown-unknown/release/block_resources.wasm \
-  --network testnet \
-  --source <TU_SECRET_KEY_MULTISIG> \
-  --permission-set-name admin  # Opcional para permisos
-echo "Contrato desplegado. ID: $(soroban contract id --network testnet --source <TU_SECRET_KEY_MULTISIG> --wasm target/wasm32-unknown-unknown/release/block_resources.wasm)"
+  --source <TU_PUBLIC_MULTISIG> \
+  --network testnet
+echo "Contrato desplegado. ID: $(stellar contract id --network testnet --source <TU_PUBLIC_MULTISIG> --wasm target/wasm32-unknown-unknown/release/block_resources.wasm)"
